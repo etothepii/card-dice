4f27b23c0224229a1ec6ff7c9eb16e3e9ed3a46e
James Robinson <james@bermuda.earth>
2019-10-27 18:34:34 -0700

    Empty project from xcode

---
+++ card-dice/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  card-dice
+//
+//  Created by James Robinson on 27/10/2019.
+//  Copyright © 2019 James Robinson. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
